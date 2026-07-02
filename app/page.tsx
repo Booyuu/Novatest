a13119eea7e2bcb1a3b058bdf4e92131f0348d36
Boyu <Booyuu@proton.me>
2026-07-02 13:52:27 +0800

    Add enterprise strength section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { CTA } from '@/components/CTA';
 import { Footer } from '@/components/Footer';
 import { Hero } from '@/components/Hero';
 import { HomeDeepSections } from '@/components/HomeDeepSections';
+import { HomeEnterpriseStrength } from '@/components/HomeEnterpriseStrength';
 import { MaintenanceModal } from '@/components/MaintenanceModal';
 import { Navbar } from '@/components/Navbar';
 
@@ -17,6 +18,7 @@ export default function Home() {
       <Navbar onOpenModal={openModal} />
       <Hero onOpenModal={openModal} />
       <HomeDeepSections onOpenModal={openModal} />
+      <HomeEnterpriseStrength />
       <CTA onOpenModal={openModal} />
       <Footer onOpenModal={openModal} />
       <MaintenanceModal open={modalOpen} onClose={() => setModalOpen(false)} />
